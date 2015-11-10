4360407aba166fc2fe94ba678949cef4da681032
SladeThe <sladethe@gmail.com>
2015-11-10 18:44:19 +0300

    - fixed RemoteProcessClient.cpp for big-endian systems.

--- RemoteProcessClient.cpp
+++ RemoteProcessClient.cpp
@@ -853,7 +853,7 @@ int RemoteProcessClient::readInt() {
     vector<signed char> bytes = this->readBytes(INTEGER_SIZE_BYTES);
 
     if (this->isLittleEndianMachine() != LITTLE_ENDIAN_BYTE_ORDER) {
-        reverse(&bytes[0], &bytes[INTEGER_SIZE_BYTES - 1]);
+        reverse(bytes.begin(), bytes.end());
     }
 
     int value;
@@ -910,7 +910,7 @@ void RemoteProcessClient::writeInt(int value) {
     memcpy(&bytes[0], &value, INTEGER_SIZE_BYTES);
 
     if (this->isLittleEndianMachine() != LITTLE_ENDIAN_BYTE_ORDER) {
-        reverse(&bytes[0], &bytes[INTEGER_SIZE_BYTES - 1]);
+        reverse(bytes.begin(), bytes.end());
     }
 
     this->writeBytes(bytes);
@@ -938,7 +938,7 @@ long long RemoteProcessClient::readLong() {
     vector<signed char> bytes = this->readBytes(LONG_SIZE_BYTES);
 
     if (this->isLittleEndianMachine() != LITTLE_ENDIAN_BYTE_ORDER) {
-        reverse(&bytes[0], &bytes[LONG_SIZE_BYTES - 1]);
+        reverse(bytes.begin(), bytes.end());
     }
 
     long long value;
@@ -954,7 +954,7 @@ void RemoteProcessClient::writeLong(long long value) {
     memcpy(&bytes[0], &value, LONG_SIZE_BYTES);
 
     if (this->isLittleEndianMachine() != LITTLE_ENDIAN_BYTE_ORDER) {
-        reverse(&bytes[0], &bytes[LONG_SIZE_BYTES - 1]);
+        reverse(bytes.begin(), bytes.end());
     }
 
     this->writeBytes(bytes);
